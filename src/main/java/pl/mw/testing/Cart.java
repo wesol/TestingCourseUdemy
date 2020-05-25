bf86af248a828e9601d9e0decfa452a0d24a5305
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-05-25 18:28:38 +0200

    e.21 Badanie wydajności - timeout

---
+++ src/main/java/pl/mw/testing/Cart.java
@@ -0,0 +1,28 @@
+package pl.mw.testing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+
+    private final List<Order> orders = new ArrayList<>();
+
+    void addOrderToCard(Order order) {
+        this.orders.add(order);
+    }
+
+    void clearCard() {
+        this.orders.clear();
+    }
+
+    void simulateLargeOrder() {
+        for (int i = 0; i < 1_000; i++) {
+            Meal meal = new Meal(10, "Meal no " + i);
+            Order order = new Order();
+            order.addMealToOrder(meal);
+            addOrderToCard(order);
+        }
+        System.out.println("Cart size: " + orders.size());
+        clearCard();
+    }
+}
---
+++ src/test/java/pl/mw/testing/CartTest.java
@@ -0,0 +1,20 @@
+package pl.mw.testing;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+class CartTest {
+
+    @Test
+    void simulateLargeOrder() {
+        // given
+        Cart cart = new Cart();
+
+        // when
+        // then
+        assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+}
