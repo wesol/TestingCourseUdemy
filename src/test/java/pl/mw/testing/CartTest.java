90acc516fab05fa19e40d1923bf95830520d1796
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-05-25 19:02:18 +0200

    e.23 Matchery do łączenia assercji

--- src/test/java/pl/mw/testing/CartTest.java
+++ src/test/java/pl/mw/testing/CartTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
 
 class CartTest {
@@ -16,5 +19,28 @@ class CartTest {
         // when
         // then
         assertTimeout(Duration.ofMillis(100), cart::simulateLargeOrder);
+    }
+
+    @Test
+    void cartShouldNotBeEmptyAfterAddedOrderToTheCart() {
+        // given
+        Order order = new Order();
+        Cart cart = new Cart();
+
+        // when
+        cart.addOrderToCard(order);
+
+        // then
+        assertThat(cart.getOrders(), anyOf(
+                notNullValue(),
+                hasSize(1),
+                is(not(empty())),
+                is(not(emptyCollectionOf(Order.class)))
+        ));
+
+        assertAll( //alternatywa dla allOf z hamcrest
+                   () -> assertThat(cart.getOrders(), notNullValue()),
+                   () -> assertThat(cart.getOrders().get(0).getMeals(), empty())
+        );
     }
 }
