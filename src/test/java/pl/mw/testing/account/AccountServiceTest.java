820271271162ceaffbb5f14cf6a9909b4dbb5b72
Mateusz Wesołowski <mwesolowski88@gmail.com>
2020-05-26 22:55:50 +0200

    e.37 Mocki - definicja i zastosowanie

---
+++ src/test/java/pl/mw/testing/account/AccountServiceTest.java
@@ -0,0 +1,57 @@
+package pl.mw.testing.account;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+
+class AccountServiceTest {
+
+    @Test
+    void getAllActiveAccounts() {
+        // given
+        List<Account> accounts = prepareAccountData();
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+        given(accountRepository.getAllAccounts()).willReturn(accounts);
+
+        // when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        // then
+        assertThat(accountList, hasSize(2));
+    }
+
+    @Test
+    void getNoActiveAccounts() {
+        // given
+        AccountRepository accountRepository = mock(AccountRepository.class);
+        AccountService accountService = new AccountService(accountRepository);
+        given(accountRepository.getAllAccounts()).willReturn(Collections.emptyList());
+
+        // when
+        List<Account> accountList = accountService.getAllActiveAccounts();
+
+        // then
+        assertThat(accountList, hasSize(0));
+    }
+
+    private List<Account> prepareAccountData() {
+
+        Address address1 = new Address("Kwiatowa", "33/5");
+        Account account1 = new Account(address1);
+
+        Account account2 = new Account();
+
+        Address address2 = new Address("Tamka", "80B");
+        Account account3 = new Account(address2);
+
+        return Arrays.asList(account1, account2, account3);
+    }
+}
